a11884ae033ccb6cf93b05b8a074532c4dd9fa45
gauravswarankar <gaurav@sparsity-technologies.com>
2023-02-11 10:06:52 +0530

    get and post method implemented

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def login():
-    return "Hello Worlddd"
+items = [
+    {
+        "name": "Green Apple",
+        "price": 200
+    },
+    {
+
+        "name": "Momos",
+        "price": 50
+    },
+]
+
+
+@app.get('/get-items')
+def get_items():
+    return {"items": items}
+
+
+@app.post('/add-items')
+def add_items():
+    request_data = request.get_json()
+    items.append(request_data)
+    return {"message": "Items added successfully"}, 201
