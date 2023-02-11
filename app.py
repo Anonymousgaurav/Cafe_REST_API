7fab4b9dd82bef29c3b34ca3090f7df7936f3f7d
gauravswarankar <gaurav@sparsity-technologies.com>
2023-02-11 10:58:00 +0530

    put and delete method implemented

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_item():
     for item in items:
         if query == item['name']:
             return item
-    return {"message": "Item not found"}
+    return {"message": "Item not found"}, 404
 
 
 @app.get('/get-items')
@@ -42,3 +42,24 @@ def add_items():
     request_data = request.get_json()
     items.append(request_data)
     return {"message": "Items added successfully"}, 201
+
+
+@app.put('/update-item')
+def update_items():
+    request_data = request.get_json()
+    for item in items:
+        if item['name'] == request_data['name']:
+            item['price'] = request_data['price']
+            return {"message": "Items updated successfully"}
+    return {"message": "Item not available"}, 404
+
+
+@app.delete('/delete-item')
+def delete_item():
+    query = request.args.get('name')
+    for item in items:
+        if query == item['name']:
+            items.remove(item)
+            return {"message": "Item deleted successfully"}, 200
+    return {"message": "Item not found"}, 404
+
